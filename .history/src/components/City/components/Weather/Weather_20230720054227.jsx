4d1c8c1785998e2ec77d2f114737fe78e6f2b767
Tracy-maker <ydlvns@gmail.com>
2023-07-20 05:58:25 +1000

    add files

---
+++ .history/src/components/City/components/Weather/Weather_20230720054227.jsx
@@ -0,0 +1,6 @@
+import Temperature from "./components/Temperature/Temperature";
+
+function Weather() {
+  return <div><Temperature/></div>;
+}
+export default Weather;
--- .history/src/components/City/components/Weather/Weather_20230720054332.jsx
+++ .history/src/components/City/components/Weather/Weather_20230720054332.jsx
@@ -1,6 +1,5 @@
 import Details from "./components/Details/Details";
 import Temperature from "./components/Temperature/Temperature";
-import Condition from "./components/Condition/Condition"
 
 function Weather() {
   return <div><Temperature/><Condition/><Details/>
