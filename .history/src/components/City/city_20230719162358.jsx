e5851d26e4169b901e958759a5896fa3ec0a97d8
Yix <ydlvns@gmail.com>
2023-07-19 16:38:29 +1000

    city file done

---
+++ .history/src/components/City/city_20230719162358.jsx
@@ -0,0 +1,35 @@
+import Weather from "./components/Weather/Weather";
+import Name from "./components/Name/Name";
+import { styled } from "styled-components";
+import { Box, Container } from "@mui/material";
+import p2 from "../../assets/background2.jpg";
+
+const CardContainer = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+  padding: 64px 96px;
+  background-image: url(${p2});
+  background-size: cover;
+  color: white;
+  position: relative;
+`;
+
+const Strip =styled(Box)`
+height:18px;
+background-color:rgba(0,0,0,.5);
+position:absolute;
+bottom:0;
+left:0;
+`;
+
+
+function City() {
+  return (
+    <CardContainer>
+      <Weather />
+      <Name />
+      <Strip />
+    </CardContainer>
+  );
+}
+export default City;
