90b32c9467f91987dc3f9a3c2c2c91dfb11b7263
Tracy-maker <ydlvns@gmail.com>
2023-08-05 19:13:07 +1000

    fix errors

---
+++ .history/src/components/OtherCities/components/City/City_20230804222808.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { Paper, Typography } from "@mui/material";
+import ConditionImage from "../ConditionImage/ConditionImage";
+
+const CityList = styled(Paper)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const CityName = styled(Typography)`
+  width: 120px;
+  color: #696969;
+  text-align: left;
+  margin-right: 0.5rem;
+  font-size: 29px;
+`;
+
+const CityTemperature = styled(Typography)`
+  width: 4rem;
+  text-align: right;
+  color: #696969;
+`;
+
+function City({ name,temperature,weather,onClicked }) {
+
+  return (
+    <CityList
+      elevation={0}
+      onClick={(event) => {
+        event.preventDefault();
+        onClicked();
+      }}
+    >
+      <CityName variant="subtitle1">{name}</CityName>
+      <CityTemperature variant="body1">{`${parseFloat(temperature).toFixed(0)}゜`}</CityTemperature>
+      <ConditionImage  weather={weather} />
+    </CityList>
+  );
+}
+export default City;
---
+++ src/components/Forecast/components/ImageIcons/ConditionImage.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Image = styled.img`
+  width: 50px;
+  height: 50px;
+`;
+
+function ConditionImage({ weather }) {
+  return (
+    <Image
+      src={`http://openweathermap.org/img/wn/${weather.icon}.png`}
+      alt={weather.main}
+    />
+  );
+}
+export default ConditionImage;
