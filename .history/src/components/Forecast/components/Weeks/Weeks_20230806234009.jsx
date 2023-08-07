599e9a4738be98405619f2c7afea8ca3872458ee
Tracy-maker <ydlvns@gmail.com>
2023-08-07 16:58:16 +1000

    fix temperature of padding left problem

--- .history/src/components/Forecast/components/Weeks/Weeks_20230806234009.jsx
+++ .history/src/components/Forecast/components/Weeks/Weeks_20230806234009.jsx
@@ -10,7 +10,7 @@ const ForeCastContainer = styled(Box)`
 `;
 
 const FutureTemperature = styled(Typography)`
-  padding-left:0.8rem;
+  padding-left:1rem;
 `;
 const DailyBox = styled(Stack)`
   justify-content: center;
